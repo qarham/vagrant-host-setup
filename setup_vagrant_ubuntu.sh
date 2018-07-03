638aca4e7b20c9e2578ab190ded990979f62fe80
Qarham <qarham@juniper.net>
2018-07-03 14:33:43 -0500

    added ubuntu vagrant script

--- setup_vagrant_ubuntu.sh
+++ setup_vagrant_ubuntu.sh
@@ -1,19 +1,39 @@
 #!/bin/bash -v
 
-echo "Basic Packages Install"
+apt-get update
+apt-get install -y wget git bridge-utils python python-pip
 
-echo "VirtualBox Install"
+# VirtualBox Installation
+# Add following line in "/etc/apt/sources.list"
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-5.2
 
+### Vagrant install
+wget https://releases.hashicorp.com/vagrant/2.1.1/vagrant_2.1.1_x86_64.deb
+dpkg -i vagrant_2.1.1_x86_64.deb
 
-echo " Vagrnat Installation"
+## Ansible Install
+sudo apt-get update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
 
-vagrant plugin install vagrant-vbguest
+## Install JunOS Ansible Module
+sudo ansible-galaxy install Juniper.junos
 
-echo "Ansible Install"
-
-
-echo "JunOS Ansible modules installation"
 pip install --upgrade pip
 pip install jxmlease
 pip install junos-eznc
 
+## vQFX Box Addition
+wget http://10.84.5.120/cs-shared/images/vagrant-boxes/vqfx-re-virtualbox.box
+wget http://10.84.5.120/cs-shared/images/vagrant-boxes/vqfx10k-pfe-virtualbox.box
+
+vagrant box add --name juniper/vqfx10k-re /var/tmp/vqfx-re-virtualbox.box
+vagrant box add --name juniper/vqfx10k-pfe /var/tmp/vqfx10k-pfe-virtualbox.box
+
+echo "List Box"
+vagrant box list
